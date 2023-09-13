78159c542f81898cfb739cae419b122f2747343c
Dushantha Olinda <2020cs185@stu.ucsc.cmb.ac.lk>
2023-09-13 09:02:21 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1>Homepage</h1>
+            <p>Lorem hgahfgs gjawgf gef jywqgfya htfwascfhga yjawg cyafc  ysfc ya ftaf thfh</p>
+        </div>
+    )
+}
+
+export default Homepage;
